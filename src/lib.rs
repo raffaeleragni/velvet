9c4411b85e3c726e61ec8ea62699e38dfde790fc
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-04-06 11:21:14 +0200

    export type for jwt

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,7 @@ pub mod prelude {
     pub use axum::routing::{delete, get, patch, post, put};
     pub use axum::{Extension, Router};
     pub use axum_extra::extract::CookieJar;
+    pub use jsonwebtoken::DecodingKey;
     pub use reqwest::Client;
     pub use rust_embed::RustEmbed;
     pub use serde::{Deserialize, Serialize};
