29f87246600a1d4d45a5c1388115146f5948bbb9
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-10-03 23:55:16 +0200

    serial and use 2 metric calls

--- tests/test.rs
+++ tests/test.rs
@@ -1,9 +1,11 @@
+use serial_test::serial;
 use velvet_web::prelude::*;
 
 mod example_app;
 use example_app::app;
 
 #[tokio::test]
+#[serial]
 async fn test() -> AppResult<()> {
     let server = app().await?.as_test_server().await;
     assert_eq!("OK", server.get("/").await.text());
@@ -17,21 +19,21 @@ async fn test() -> AppResult<()> {
 }
 
 #[tokio::test]
+#[serial]
 async fn test_custom_metrics() -> AppResult<()> {
     let server = App::new()
         .route(
             "/",
             get(|| async {
                 metric_counter("test_metric_counter").increment(1);
+                metric_counter("another_metric").increment(1);
             }),
         )
         .as_test_server()
         .await;
     server.get("/").await;
-    assert!(server
-        .get("/metrics/prometheus")
-        .await
-        .text()
-        .contains("test_metric_counter"));
+    let result = server.get("/metrics/prometheus").await.text();
+    assert!(result.contains("test_metric_counter"));
+    assert!(result.contains("another_metric"));
     Ok(())
 }
